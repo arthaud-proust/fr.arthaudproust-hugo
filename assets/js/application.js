f942bbf3aec92a5448f66f311d0d449e73583818
Arthaud <arthaudp33@gmail.com>
2023-01-06 12:02:08 +0100

    feat: clean code

--- assets/js/application.js
+++ assets/js/application.js
@@ -6,7 +6,6 @@ import { setEffects } from './shims/effects';
 import { setScrollEffects } from './shims/scrollEffects';
 import { setPaintYComponents, animePaintYComponents } from './shims/paintYEffect';
 
-
 addEventListener('turbo:before-render', function(event) {
     event.preventDefault();
     superCursor.setState(superCursor.states.COVER_PAGE)
@@ -70,113 +69,143 @@ addEventListener('DOMContentLoaded', function() {
 
 addEventListener('turbo:load', function() {
     superCursor.enable();
+
     if(!utils.mobileAndTabletCheck()) {
         superCursor.enableMouseMode()
     }
     
-    const gridGutter = 16*3;
+    window.gridGutter = 16*3;
 
     setPaintYComponents();
+
     setTimeout(()=>document.querySelectorAll('[anime-paint-y]').forEach(root=>animePaintYComponents(root)), 800);
+
     setEffects();
+
     setScrollEffects();
 
+    addCopyOnClickToCopyableLinks();
+
+    initProjectsGroupSwiper();
+
+    initTestimoniesGroupSwiperAndCollapsibles();
+
+    initProjectSectionsRoot();
+});
+
+function addCopyOnClickToCopyableLinks() {
     const linksToCopy = document.querySelectorAll('.toCopy');
-    for(let link of linksToCopy) {
-        link.addEventListener('click', function() {
-            if(!link.dataset.baseText) link.dataset.baseText = link.innerText;
-            utils.copyTextToClipboard(link.innerText)
-                .then(()=>{
-                    link.innerText = `${link.dataset.baseText} (copié)`;
-                })
-                .catch(()=>{
-                    link.innerText = `${link.dataset.baseText} (pas copié)`;
-                })
-                .finally(()=>{
-                    setTimeout(function() {
-                        link.innerText = link.dataset.baseText;
-                    }, 4000);
-                })
+
+    linksToCopy.forEach(link=>
+        link.addEventListener('click', ()=>copyLinkContentToClipboard(link))
+    );
+}
+
+function copyLinkContentToClipboard(link) {
+    if(!link.dataset.baseText) link.dataset.baseText = link.innerText;
+
+    utils.copyTextToClipboard(link.innerText)
+        .then(()=>{
+            link.innerText = `${link.dataset.baseText} (copié)`;
         })
-    }
+        .catch(()=>{
+            link.innerText = `${link.dataset.baseText} (pas copié)`;
+        })
+        .finally(()=>{
+            setTimeout(function() {
+                link.innerText = link.dataset.baseText;
+            }, 4000);
+        })
+}
 
+function initProjectsGroupSwiper() {
     const projectsGroup = document.getElementById('projects-group');
-    if(projectsGroup) {
 
-        const projectsSwiper = new Swiper(projectsGroup, {
-            direction: 'horizontal',
-            grabCursor: true,
-            loop: false,
-            resistanceRatio: 0.9,
-            pagination: {
-                el: '.swiper-pagination',
-            },
-
-            breakpoints: {
-                576: { // >= sm
-                    slidesPerView: 2,
-                    spaceBetween: gridGutter
-                },
-                992: { // >= lg
-                    slidesPerView: 2,
-                    spaceBetween: gridGutter
-                },
-
-                1200: { // >= xl
-                    slidesPerView: 2,
-                    spaceBetween: gridGutter
-                },
-            },
-            mousewheel: {
-                enabled: true,
-                releaseOnEdges: true,
-                forceToAxis: true
-            }
-        });
-        projectsSwiper.on('touchStart', function (swiper, event) {
-            superCursor.setState(superCursor.states.ACTIVE);
-        });
-        projectsSwiper.on('sliderMove', function (swiper, event) {
-            superCursor.updateMouseFromEvent(event);
-        });
-        
-        projectsSwiper.on('touchEnd', function (swiper, event) {
-            superCursor.setState(superCursor.states.NORMAL);
-        });
+    if(!projectsGroup) {
+        return;
     }
 
+    const projectsSwiper = new Swiper(projectsGroup, {
+        direction: 'horizontal',
+        grabCursor: true,
+        loop: false,
+        resistanceRatio: 0.9,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        breakpoints: {
+            576: { // >= sm
+                slidesPerView: 2,
+                spaceBetween: gridGutter
+            },
+            992: { // >= lg
+                slidesPerView: 2,
+                spaceBetween: gridGutter
+            },
+
+            1200: { // >= xl
+                slidesPerView: 2,
+                spaceBetween: gridGutter
+            },
+        },
+        mousewheel: {
+            enabled: true,
+            releaseOnEdges: true,
+            forceToAxis: true
+        }
+    });
+
+    projectsSwiper.on('touchStart', function (swiper, event) {
+        superCursor.setState(superCursor.states.ACTIVE);
+    });
+
+    projectsSwiper.on('sliderMove', function (swiper, event) {
+        superCursor.updateMouseFromEvent(event);
+    });
+
+    projectsSwiper.on('touchEnd', function (swiper, event) {
+        superCursor.setState(superCursor.states.NORMAL);
+    });
+}
+
+function initTestimoniesGroupSwiperAndCollapsibles() {
     const testimoniesGroup = document.getElementById('testimonies-group');
-    if(testimoniesGroup) {
-        const testimoniesSwiper = new Swiper(testimoniesGroup, {
-            grabCursor: true,
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-            },
-            autoplay: {
-                delay: 5000,
-                disableOnInteraction: true,
-            },
-            resistanceRatio: 0.9,
-            spaceBetween: 16*1.5,
-            mousewheel: {
-                enabled: true,
-                releaseOnEdges: true,
-                forceToAxis: true
-            }
-        });
 
-        testimoniesSwiper.on('touchStart', function (swiper, event) {
-            superCursor.setState(superCursor.states.ACTIVE);
-        });
-        testimoniesSwiper.on('sliderMove', function (swiper, event) {
-            superCursor.updateMouseFromEvent(event);
-        });
-        
-        testimoniesSwiper.on('touchEnd', function (swiper, event) {
-            superCursor.setState(superCursor.states.NORMAL);
-        });
+    if(!testimoniesGroup) {
+        return
     }
+
+    const testimoniesSwiper = new Swiper(testimoniesGroup, {
+        grabCursor: true,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: true,
+        },
+        resistanceRatio: 0.9,
+        spaceBetween: 16*1.5,
+        mousewheel: {
+            enabled: true,
+            releaseOnEdges: true,
+            forceToAxis: true
+        }
+    });
+
+    testimoniesSwiper.on('touchStart', function (swiper, event) {
+        superCursor.setState(superCursor.states.ACTIVE);
+    });
+
+    testimoniesSwiper.on('sliderMove', function (swiper, event) {
+        superCursor.updateMouseFromEvent(event);
+    });
+
+    testimoniesSwiper.on('touchEnd', function (swiper, event) {
+        superCursor.setState(superCursor.states.NORMAL);
+    });
 
     const testimoniesCollapsibles = document.querySelectorAll('.testimonyCollapsible')
     testimoniesCollapsibles.forEach(collapse=>{
@@ -190,39 +219,44 @@ addEventListener('turbo:load', function() {
             button.innerText = "Lire moins"
         })
     });
+}
 
+function initProjectSectionsRoot() {
     const projectSectionsRoot = document.getElementById('project-sections-root');
-    if(projectSectionsRoot) {
-        const projectSections = document.querySelectorAll('.project-section')
-        const projectImages = document.querySelectorAll('.project-image')
-        const y = Math.round(window.innerHeight * 0.7)
-        const rootMargin = `-${window.innerHeight - y - 1}px 0px -${y}px 0px`;
-        const options = {
-            rootMargin,
-        }
-        
-        function showProjectImageOfSection(section) {
-            for(let image of projectImages) {
-                if(image.id !== section.dataset.image) {
-                    image.classList.add('d-none');
-                } else {
-                    image.classList.remove('d-none');
-                }
+
+    if(!projectSectionsRoot) {
+        return
+    }
+
+    const projectSections = document.querySelectorAll('.project-section')
+    const projectImages = document.querySelectorAll('.project-image')
+    const y = Math.round(window.innerHeight * 0.7)
+    const rootMargin = `-${window.innerHeight - y - 1}px 0px -${y}px 0px`;
+    const options = {
+        rootMargin,
+    }
+
+    function showProjectImageOfSection(section) {
+        for(let image of projectImages) {
+            if(image.id !== section.dataset.image) {
+                image.classList.add('d-none');
+            } else {
+                image.classList.remove('d-none');
             }
-        }
-
-        function handleSectionIntersect(entries, observer) {
-            for(let entry of entries) {
-                if (entry.isIntersecting) {
-                    showProjectImageOfSection(entry.target)
-                }
-            }
-        }
-
-        const observer = new IntersectionObserver(handleSectionIntersect, options);
-
-        for(let section of projectSections) {
-            observer.observe(section)
         }
     }
-});
+
+    function handleSectionIntersect(entries) {
+        for(let entry of entries) {
+            if (entry.isIntersecting) {
+                showProjectImageOfSection(entry.target)
+            }
+        }
+    }
+
+    const observer = new IntersectionObserver(handleSectionIntersect, options);
+
+    for(let section of projectSections) {
+        observer.observe(section)
+    }
+}
